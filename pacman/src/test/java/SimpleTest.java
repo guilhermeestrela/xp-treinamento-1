6d66b9dc5b319da883a70337e6ff6dfeea8b1be1
Alan Souza <alansouza@MacBook-Pro-de-Alan.local>
2019-09-12 09:53:50 -0300

    Add test

---
+++ pacman/src/test/java/SimpleTest.java
@@ -0,0 +1,14 @@
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+
+
+    @Test
+    void addition() {
+        assertEquals(2, 1 + 1);
+    }
+}
+
