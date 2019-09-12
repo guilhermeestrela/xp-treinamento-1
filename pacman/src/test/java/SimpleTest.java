6702529cb88b8b51a6b449f529d2eaae978d0480
Gilson Vieira <gilson.vieira@clickbus.com>
2019-09-12 11:56:30 -0300

    Added dummy unit tests

--- pacman/src/test/java/SimpleTest.java
+++ pacman/src/test/java/SimpleTest.java
@@ -1,5 +1,4 @@
-
-
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 import pacman.Jogo;
 import pacman.Pacboy;
@@ -8,16 +7,72 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimpleTest {
 
-    Jogo jogo = new Pacboy();
-
     @Test
+    @Ignore
     void telaInicial(){
+        Jogo jogo = new Pacboy();
 
-        assertEquals("                    " + "\n" +
-                "                    " + "\n" +
-                "                    " + "\n" +
-                "                    " + "\n" +
-                "                    " + "\n", jogo.tela());
+        assertEquals("     " + "\n" +
+                "     " + "\n" +
+                "     " + "\n" +
+                "     " + "\n" +
+                "     " + "\n" , jogo.tela());
 
     }
+
+    @Test
+    @Ignore
+    void testPacboyNaTela() {
+        Jogo jogo = new Pacboy();
+        assertEquals("     " + "\n" +
+                "     " + "\n" +
+                "  C  " + "\n" +
+                "     " + "\n" +
+                "     " + "\n" , jogo.tela());
+    }
+
+    @Test
+    @Ignore
+    void testPacboyDireita() {
+        Jogo jogo = new Pacboy();
+        assertEquals("     " + "\n" +
+                "     " + "\n" +
+                "   C " + "\n" +
+                "     " + "\n" +
+                "     " + "\n" , jogo.tela());
+    }
+
+    @Test
+    @Ignore
+    void testPacboyEsquerda() {
+        Jogo jogo = new Pacboy();
+        assertEquals("     " + "\n" +
+                "     " + "\n" +
+                " C   " + "\n" +
+                "     " + "\n" +
+                "     " + "\n" , jogo.tela());
+    }
+
+    @Test
+    @Ignore
+    void testPacboyBaixo() {
+        Jogo jogo = new Pacboy();
+        assertEquals("     " + "\n" +
+                "     " + "\n" +
+                "     " + "\n" +
+                "  C  " + "\n" +
+                "     " + "\n" , jogo.tela());
+    }
+
+    @Test
+    @Ignore
+    void testPacboyCima() {
+        Jogo jogo = new Pacboy();
+        assertEquals("     " + "\n" +
+                "  C  " + "\n" +
+                "     " + "\n" +
+                "     " + "\n" +
+                "     " + "\n", jogo.tela());
+    }
+
 }
