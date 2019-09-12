d5e395175685a51b317768caed9d65b4003f0838
Bruno Silva <bruno.silva@clickbus.com>
2019-09-12 15:29:48 -0300

    teste com labirinto

--- pacman/src/test/java/SimpleTest.java
+++ pacman/src/test/java/SimpleTest.java
@@ -13,9 +13,9 @@ public class SimpleTest {
     public void telaInicial(){
         Jogo jogo = new Pacboy();
         assertEquals("*****" + "\n" +
-                "*****" + "\n" +
-                "**C**" + "\n" +
-                "*****" + "\n" +
+                "***|*" + "\n" +
+                "*|C**" + "\n" +
+                "**|**" + "\n" +
                 "*****" + "\n", jogo.tela());
 
     }
@@ -28,9 +28,9 @@ public class SimpleTest {
         jogo.tela();
         jogo.direita();
         assertEquals("*****" + "\n" +
-                "*****" + "\n" +
-                "** C*" + "\n" +
-                "*****" + "\n" +
+                "***|*" + "\n" +
+                "*| C*" + "\n" +
+                "**|**" + "\n" +
                 "*****" + "\n", jogo.tela());
     }
 
@@ -39,11 +39,12 @@ public class SimpleTest {
     public void testPacboyEsquerda() {
         Jogo jogo = new Pacboy();
         jogo.tela();
+        jogo.sobe();
         jogo.esquerda();
         assertEquals("*****" + "\n" +
-                "*****" + "\n" +
-                "*C **" + "\n" +
-                "*****" + "\n" +
+                "*C |*" + "\n" +
+                "*| **" + "\n" +
+                "**|**" + "\n" +
                 "*****" + "\n", jogo.tela());
     }
 
@@ -52,11 +53,12 @@ public class SimpleTest {
     public void testPacboyBaixo() {
         Jogo jogo = new Pacboy();
         jogo.tela();
+        jogo.direita();
         jogo.desce();
         assertEquals("*****" + "\n" +
-                "*****" + "\n" +
-                "** **" + "\n" +
-                "**C**" + "\n" +
+                "***|*" + "\n" +
+                "*|  *" + "\n" +
+                "**|C*" + "\n" +
                 "*****" + "\n", jogo.tela());
     }
 
@@ -67,9 +69,9 @@ public class SimpleTest {
         jogo.tela();
         jogo.sobe();
         assertEquals("*****" + "\n" +
-                "**C**" + "\n" +
-                "** **" + "\n" +
-                "*****" + "\n" +
+                "**C|*" + "\n" +
+                "*| **" + "\n" +
+                "**|**" + "\n" +
                 "*****" + "\n", jogo.tela());
     }
 
@@ -80,9 +82,9 @@ public class SimpleTest {
         jogo.tela();
         jogo.direita();
         assertEquals("*****" + "\n" +
-                "*****" + "\n" +
-                "** C*" + "\n" +
-                "*****" + "\n" +
+                "***|*" + "\n" +
+                "*| C*" + "\n" +
+                "**|**" + "\n" +
                 "*****" + "\n", jogo.tela());
     }
 
@@ -93,9 +95,9 @@ public class SimpleTest {
         jogo.tela();
         jogo.tick();
         assertEquals("M****" + "\n" +
-                "*****" + "\n" +
-                "**C**" + "\n" +
-                "*****" + "\n" +
+                "***|*" + "\n" +
+                "*|C**" + "\n" +
+                "**|**" + "\n" +
                 "*****" + "\n", jogo.tela());
     }
 
@@ -107,9 +109,9 @@ public class SimpleTest {
         jogo.tick();
         jogo.tick();
         assertEquals("*****" + "\n" +
-                "M****" + "\n" +
-                "**C**" + "\n" +
-                "*****" + "\n" +
+                "M**|*" + "\n" +
+                "*|C**" + "\n" +
+                "**|**" + "\n" +
                 "*****" + "\n", jogo.tela());
     }
 
