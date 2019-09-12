402b74be9c9cdb944240013d5b43b31e30075b92
Bruno Silva <bruno.silva@clickbus.com>
2019-09-12 14:34:44 -0300

    tirando ignore

--- pacman/src/test/java/SimpleTest.java
+++ pacman/src/test/java/SimpleTest.java
@@ -22,59 +22,59 @@ public class SimpleTest {
 
 
     @Test
-    @Ignore
+
     public void testPacboyDireita() {
         Jogo jogo = new Pacboy();
         jogo.tela();
         jogo.direita();
         assertEquals("*****" + "\n" +
                 "*****" + "\n" +
-                "***C*" + "\n" +
+                "** C*" + "\n" +
                 "*****" + "\n" +
                 "*****" + "\n", jogo.tela());
     }
 
     @Test
-    @Ignore
+
     public void testPacboyEsquerda() {
         Jogo jogo = new Pacboy();
         jogo.tela();
         jogo.esquerda();
         assertEquals("*****" + "\n" +
                 "*****" + "\n" +
-                "*C***" + "\n" +
+                "*C **" + "\n" +
                 "*****" + "\n" +
                 "*****" + "\n", jogo.tela());
     }
 
     @Test
-    @Ignore
+
     public void testPacboyBaixo() {
         Jogo jogo = new Pacboy();
         jogo.tela();
         jogo.desce();
         assertEquals("*****" + "\n" +
                 "*****" + "\n" +
-                "*****" + "\n" +
+                "** **" + "\n" +
                 "**C**" + "\n" +
                 "*****" + "\n", jogo.tela());
     }
 
     @Test
-    @Ignore
+
     public void testPacboyCima() {
         Jogo jogo = new Pacboy();
         jogo.tela();
         jogo.sobe();
         assertEquals("*****" + "\n" +
                 "**C**" + "\n" +
-                "*****" + "\n" +
+                "** **" + "\n" +
                 "*****" + "\n" +
                 "*****" + "\n", jogo.tela());
     }
 
     @Test
-    @Ignore
+
     public void testPacboyComeBolinha() {
         Jogo jogo = new Pacboy();
         jogo.tela();
